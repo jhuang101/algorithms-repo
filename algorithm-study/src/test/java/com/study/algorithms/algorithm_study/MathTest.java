628f8e5bed151a6b69eab2be625f1093a23e861a
Jia Huang <jiayanhuang@yahoo.com>
2018-03-13 10:43:58 -0700

    revise class

--- algorithm-study/src/test/java/com/study/algorithms/algorithm_study/MathTest.java
+++ algorithm-study/src/test/java/com/study/algorithms/algorithm_study/MathTest.java
@@ -2,21 +2,16 @@ package com.study.algorithms.algorithm_study;
 
 import static org.junit.Assert.*;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class MathTest{
 	@Test
 	public void testGCD(){
-		Math math = new Math();
-		long gcd = math.gcd(1,3);
-		Assert.assertEquals(1,gcd);
+		assertEquals(1,Math.gcd(1, 3));
 	}
 	@Test
 	public void testLCM(){
-		Math math = new Math();
-		long lcm = math.lcm(3,5);
-		Assert.assertEquals(15,lcm);
+		assertEquals(15,Math.lcm(3,5));
 	}
 	@Test
 	public void testDummy(){
@@ -25,6 +20,6 @@ public class MathTest{
 	
 	@Test
 	public void testx(){
-		assertEquals('b','b'); 
+		assertEquals('b','b');
 	}
 }
